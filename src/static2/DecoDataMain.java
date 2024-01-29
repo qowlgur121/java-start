bab55993c6d693f3a7a43967049b02f0eb6394b3
Jihyeok <qowlgur121@gmail.com>
2024-01-29 19:36:53 +0900

    static2 패키지 클래스 추가

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,17 @@
 package static2;
 
+//import static static2.DecoData.staticCall;
+//import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1.정적 호출");
         DecoData.staticCall();
+        DecoData.staticCall();
+        DecoData.staticCall();
+        DecoData.staticCall();
+        DecoData.staticCall();
+
 
         System.out.println("2.인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -12,5 +20,13 @@ public class DecoDataMain {
         System.out.println("3.인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        //추가
+        //인스턴스를 통한 접근. 권장 안함
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        //클래스를 통한 접근. 권장
+        DecoData.staticCall();
     }
 }
