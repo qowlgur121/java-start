fe05b9173034fede94ae0ba2d407c9120c41fc3c
Jihyeok <qowlgur121@gmail.com>
2024-02-02 19:20:50 +0900

    poly.car2 패키지 추가

---
+++ src/poly/car1/Driver.java
@@ -0,0 +1,18 @@
+package poly.car1;
+
+public class Driver {
+    //클라이언트 쪽은 수정하지 않게 짜자
+    private Car car;
+
+    public void setCar(Car car) {
+        System.out.println("자동차를 설정합니다: " + car);
+        this.car = car;
+    }
+
+    public void drive() {
+        System.out.println("자동차를 운전합니다.");
+        car.startEngine();
+        car.pressAccelerator();
+        car.offEngine();
+    }
+}
