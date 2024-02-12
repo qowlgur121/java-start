cb3471802c5bc20608dff9a9524d2aa6fb767796
Jihyeok <qowlgur121@gmail.com>
2024-02-12 23:38:35 +0900

    HashSet 업로드

---
+++ src/collection/HashSetExample.java
@@ -0,0 +1,21 @@
+package collection;
+
+import access.Speaker;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+        set.add("java");
+        set.add("JDBC");
+        set.add("Java");
+        set.add("JSP");
+        set.add("Spring");
+
+        int size = set.size();
+        System.out.println("총 객체 수: " + size);
+    }
+}
