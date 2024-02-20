ae1535a61583506ffe17645648c4ab6f09fce886
Jihyeok <qowlgur121@gmail.com>
2024-02-20 23:45:40 +0900

    람다 추가

---
+++ src/lambda/LamdaEx1.java
@@ -0,0 +1,23 @@
+package lambda;
+
+
+public class LamdaEx1 {
+    public static void main(String[] args) {
+        action((x, y) -> {
+            int result = x + y;
+            System.out.println("result: " + result);
+        });
+
+        action(((x, y) -> {
+            int result = x - y;
+            System.out.println("result: " + result);
+        }));
+    }
+
+    private static void action(Calculable calculable) {
+        //데이터
+        int x = 10;
+        int y = 4;
+        calculable.calculate(x, y);
+    }
+}
