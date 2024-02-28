0e9069b2c4c9664ba5e61f0d3f2cc10ebff654b0
Jihyeok <qowlgur121@gmail.com>
2024-02-28 23:26:20 +0900

    스트림 예제 추가

---
+++ src/stream/StreamEx1.java
@@ -0,0 +1,19 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamEx1 {
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList("Apple", "Banana", "Cherry", "Durian", "Elderberry");
+
+        List<String> longWords = words.stream()
+                .filter(word -> word.length() >= 5)
+                .collect(Collectors.toList());
+
+        System.out.println(longWords); // 출력: [Banana, Cherry, Durian, Elderberry]
+
+    }
+}
