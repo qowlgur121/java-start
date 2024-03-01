f0af903192474db36e5ff766eb779d38e3a95f97
Jihyeok <qowlgur121@gmail.com>
2024-03-01 23:41:02 +0900

    스트림 예제 추가

---
+++ src/stream/StreamExample.java
@@ -0,0 +1,29 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // 1. 스트림 생성 및 필터링
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println("Even numbers: " + evenNumbers);
+
+        // 2. 매핑
+        List<Integer> squaredNumbers = numbers.stream()
+                .map(n -> n * n)
+                .collect(Collectors.toList());
+        System.out.println("Squared numbers: " + squaredNumbers);
+
+        // 3. 리듀싱
+        int sum = numbers.stream()
+                .reduce(0, Integer::sum);
+        System.out.println("Sum: " + sum);
+    }
+}
+
