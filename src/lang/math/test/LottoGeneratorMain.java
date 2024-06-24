6edbc828a49fba32236b685ea430a1e71e4ea9c3
Jihyeok <qowlgur121@gmail.com>
2024-06-25 02:13:15 +0900

    테스트 코드

---
+++ src/lang/math/test/LottoGeneratorMain.java
@@ -0,0 +1,13 @@
+package lang.math.test;
+
+public class LottoGeneratorMain {
+    public static void main(String[] args) {
+        LottoGenerator generator = new LottoGenerator();
+        int[] lottoNumbers = generator.generate();
+
+        System.out.print("로또 번호: ");
+        for (int lottoNumber : lottoNumbers) {
+            System.out.print(lottoNumber + " ");
+        }
+    }
+}
